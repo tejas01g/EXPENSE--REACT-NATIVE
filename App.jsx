f5bdbbb046c52cf223b39dc698e9840f35fc6434
tejas01g <gargtejasvi076@gmail.com>
2025-07-07 15:57:24 +0530

    n

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import Welcome from './src/Welcome'
-import Home from './src/Home'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Welcome from './src/Welcome';
+import Home from './src/Home';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Profile from './src/Profile';
 // import { create } from 'react-native/types_generated/Libraries/ReactNative/ReactFabricPublicInstance/ReactNativeAttributePayload'
 
 const Stack = createNativeStackNavigator();
@@ -11,16 +12,18 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName = "Welcome" screenOptions = {{headerShown:false}}>
-        <Stack.Screen name = 'Welcome' component = {Welcome}/>
-        <Stack.Screen name = 'Home' component = {Home}/>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Profile} />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({});
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,117 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
-const Home = () => {
+
+
+const Home = ({navigation}) => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.text}>Home</Text>
 
-export default Home
+        <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+        <Image
+          source={{
+            uri: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          }}
+          style={styles.profileimage}
+          
+        />
+        </TouchableOpacity>
+      </View>
+
+      <View style = {styles.chartContainer}>
+        <AnimatedCircularProgress
+        size={180}
+        width={20}
+        fill={45}
+        tintColor='#00e0ff'
+        tintColorSecondary = '#8e2de2'
+        backgroundColor='#2d2f4a'
+        arcSweepAngle={360}
+        rotation={135}
+        lineCap='round'
+        >
+            {() =>(
+                <View style = {styles.innercontent}>
+                    <Text style = {styles.balance}>$11889</Text>
+                    <Text style ={styles.label}>Available Balance</Text>
+                </View>
+            )}
+        </AnimatedCircularProgress>
+      </View>
+
+      <View style ={styles.thought}>
+        <Text style = {styles.thoughttxt}>Tip of the day</Text>
+
+        <Text style = {styles.thoughtdata}>Prepare a Budget and Abide by it</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    fontSize: 25,
+  },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 25,
+    paddingTop: 29,
+  },
+  profileimage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    borderWidth: 3,
+    borderColor: '#4caf50',
+  },
+
+  chartContainer:{
+    marginTop: 40,
+    alignItems:'center',
+  },
+
+  innercontent:{
+    alignItems:'center',
+  },
+  balance:{
+    fontSize:22,
+    fontWeight:'bold',
+    color:'navyblue',
+  },
+  label:{
+    fontSize:14,
+    color: 'black'
+  },
+
+  thought:{
+    marginLeft:'25',
+    marginTop:'15',
+    width:'85%',
+    height:'10%',
+    // backgroundColor:'green',
+    // alignItems:'center'
+    paddingLeft:'14',
+    borderWidth:1,
+    borderColor:'blue',
+    borderRadius:15,
+    gap:30
+},
+  thoughttxt:{
+    fontSize:14
+  },
+  thoughtdata:{
+
+  }
+});
